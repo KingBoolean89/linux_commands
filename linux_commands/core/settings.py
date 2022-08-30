919f1ece7788609039a0cc4eb919e48d84fdbd4f
KingBoolean89 <aking893.ak@gmail.com>
2022-08-30 13:01:07 -0400

    environment variable setup

--- linux_commands/core/settings.py
+++ linux_commands/core/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-593$ab8(nu^l^$1-i=*h#%)kk_kaqpdnc0m!&66ndu3e-)1o3r'
+SECRET_KEY = os.environ["secret_key"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
